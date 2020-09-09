74fc2444e2eb93bd1b6e0deaffd3d590281836f8
slurio <samantha.lurio@gmail.com>
2020-09-08 21:51:18 -0400

    working on creating a favorite for user

--- app/controllers/user_arts_controller.rb
+++ app/controllers/user_arts_controller.rb
@@ -1,7 +1,23 @@
 class UserArtsController < ApplicationController
 
     def new
-      byebug  
+    @art_data = {
+        "image" => params["image"],
+        "url_link" => params["url_link"],
+        "title" => params["title"]
+    }
+    @user_art = UserArt.new
     end
     
+    def create
+        #@user_art_data = params["user_art"]
+        @UserArt = UserArt.create(user_art_params)
+        byebug
+    end
+
+    private
+
+    def user_art_params
+        params.require(:user_art).permit(:image, :url_link, :title, :blurb, :user_id)
+    end
 end
