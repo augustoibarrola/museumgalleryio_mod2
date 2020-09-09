e81113b9b3b3aa222492a6227aa0d565b5c1545a
slurio <samantha.lurio@gmail.com>
2020-09-09 17:33:26 -0400

    index user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,11 @@ class UsersController < ApplicationController
     end
 
     def show
-       @user = User.find_by(id: session[:user_id])   
+       #@user = User.find_by(id: session[:user_id])   
+    end
+
+    def viewother
+        @user = User.find(params[:id])
     end
 
     def new
