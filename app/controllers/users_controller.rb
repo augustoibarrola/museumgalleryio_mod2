92570bfb1bddb661981368c4945818ab38e144d9
slurio <samantha.lurio@gmail.com>
2020-09-09 16:26:43 -0400

    user index page started

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
     skip_before_action :authorized, only: [:new, :create]
 
+
+    def index
+        @users = User.all
+    end
+
     def show
        @user = User.find_by(id: session[:user_id])   
     end
