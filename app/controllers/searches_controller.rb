cdb1a77cf3767b0467ccdd63795fe396c3cc5e23
slurio <samantha.lurio@gmail.com>
2020-09-10 12:08:23 -0400

    updated nav bar

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,35 @@ class SearchesController < ApplicationController
     skip_before_action :authorized, only: [:new, :show]
 
     def new
+        my_token = ENV["TOKEN"]
+        response = RestClient.get "https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getOnDisplay&access_token=" + my_token
+        json = JSON.parse response 
+        object_id = json["objects"].sample["id"]
+        object_response = RestClient.get "https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getInfo&access_token=#{my_token}&object_id=#{object_id}"
+        object_json = JSON.parse object_response
+        @one = object_json["object"]
+
+        second_response = RestClient.get "https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getOnDisplay&access_token=" + my_token
+        second_json = JSON.parse second_response 
+        second_id = second_json["objects"].sample["id"]
+        second_object_response = RestClient.get "https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getInfo&access_token=#{my_token}&object_id=#{second_id}"
+        second = JSON.parse second_object_response
+        @second = second["object"]
+
+        third_response = RestClient.get "https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getOnDisplay&access_token=" + my_token
+        third_json = JSON.parse third_response 
+        third_id = third_json["objects"].sample["id"]
+        third_object_response = RestClient.get "https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getInfo&access_token=#{my_token}&object_id=#{third_id}"
+        third = JSON.parse third_object_response
+        @third = third["object"]
+
+        fourth_response = RestClient.get "https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getOnDisplay&access_token=" + my_token
+        fourth_json = JSON.parse fourth_response 
+        fourth_id = fourth_json["objects"].sample["id"]
+        fourth_object_response = RestClient.get "https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getInfo&access_token=#{my_token}&object_id=#{fourth_id}"
+        fourth = JSON.parse fourth_object_response
+        @fourth = fourth["object"]
+
     end
 
     def show
--- app/controllers/user_arts_controller.rb
+++ app/controllers/user_arts_controller.rb
@@ -1,4 +1,5 @@
 class UserArtsController < ApplicationController
+    skip_before_action :authorized, only: [:discover]
 
     def show
         @user_art = UserArt.find(params[:id])
@@ -21,6 +22,16 @@ class UserArtsController < ApplicationController
     end
 
     def discover
+        my_token = ENV["TOKEN"]
+        response = RestClient.get "https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getOnDisplay&access_token=" + my_token
+        json = JSON.parse response 
+        object_id = json["objects"].sample["id"]
+        object_response = RestClient.get "https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getInfo&access_token=#{my_token}&object_id=#{object_id}"
+        object_json = JSON.parse object_response
+        @art = object_json["object"]
+    end
+
+    def logged_discover
         my_token = ENV["TOKEN"]
         response = RestClient.get "https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getOnDisplay&access_token=" + my_token
         json = JSON.parse response 
